2fd88533e2ccbdf3309ac92c489dcacfae4e93b5
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-09-24 10:04:39 -0700

    Programmatically set the text to display on Android

--- app/build/generated/source/r/debug/matthewallenlinsoftware/keepy_uppy/R.java
+++ app/build/generated/source/r/debug/matthewallenlinsoftware/keepy_uppy/R.java
@@ -1694,8 +1694,8 @@ containing a value of this type.
         public static final int abc_toolbar_collapse_description=0x7f060010;
         public static final int action_settings=0x7f060011;
         public static final int app_name=0x7f060012;
-        public static final int hello_world=0x7f060013;
-        public static final int status_bar_notification_info_overflow=0x7f060014;
+        public static final int status_bar_notification_info_overflow=0x7f060013;
+        public static final int textView=0x7f060014;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat=0x7f080073;
--- app/src/main/java/matthewallenlinsoftware/keepy_uppy/MainActivity.java
+++ app/src/main/java/matthewallenlinsoftware/keepy_uppy/MainActivity.java
@@ -1,16 +1,23 @@
 package matthewallenlinsoftware.keepy_uppy;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView mainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //1. Access the TextView defined in layout XML
+        // and then set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
     }
 
     @Override
