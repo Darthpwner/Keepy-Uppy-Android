7c4de528c7f0e4bd33e751284e878589c5f59e5e
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-12-20 20:47:36 -0800

    App looks like crap

--- app/src/main/java/matthewallenlinsoftware/keepy_uppy/MainActivity.java
+++ app/src/main/java/matthewallenlinsoftware/keepy_uppy/MainActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
